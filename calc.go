4d4f25d47965aaba8a551fe80bb5b1dc4ae85bca
gaoweihua <811055695@qq.com>
2021-04-08 15:16:37 +0800

    commit

---
+++ calc.go
@@ -0,0 +1,13 @@
+package calc  // package 一定要放在最上面
+
+func Add(x, y int) int {  // 首字母大写表示公有方法
+	return x + y
+}
+
+func Sub(x, y int) int {
+	return x - y
+}
+
+type Jiegouti struct  {
+	name string
+}
---
+++ test.go
@@ -0,0 +1,5 @@
+package calc  // package 一定要放在最上面
+
+func Test (i int) int {
+	return i
+}
